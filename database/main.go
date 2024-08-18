1fdb222b9058f20a6117f6c9650152bcad984615
victorthecreative <vitaopoter@hotmail.com>
2024-08-18 15:04:30 -0300

    criada as funcs para trazer apenas um registro ou mais

--- database/main.go
+++ database/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
 )
@@ -33,6 +34,25 @@ func main() {
 		panic(err)
 	}
 
+	product.Price = 2200.00
+	err = updateProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+
+	//p, err := selectProdut(db, product.ID)
+	//if err != nil {
+	//	panic(err)
+	//}
+	products, err := selectAllProducts(db)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, product := range products {
+		fmt.Printf("Product: %v, esta custando: %.2f\n", product.Name, product.Price)
+	}
+
 }
 
 func insertProduct(db *sql.DB, product *Product) error {
@@ -47,4 +67,53 @@ func insertProduct(db *sql.DB, product *Product) error {
 		return err
 	}
 	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("update products set name=?, price=? where id=?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func selectProdut(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("select * from products where id=?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	var product Product
+
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price)
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
+
+func selectAllProducts(db *sql.DB) ([]Product, error) {
+	rows, err := db.Query("select * from products")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var products []Product
+
+	for rows.Next() {
+		var product Product
+		err = rows.Scan(&product.ID, &product.Name, &product.Price)
+		if err != nil {
+			panic(err)
+		}
+		products = append(products, product)
+	}
+	return products, nil
 }
