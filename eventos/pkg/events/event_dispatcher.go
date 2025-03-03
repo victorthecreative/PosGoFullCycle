b1b29cbd1762980a2b7e99c3a140f33eedef1908
victorthecreative <vitaopoter@hotmail.com>
2025-03-03 20:46:39 -0300

    criando o meio de registro dos eventos

--- eventos/pkg/events/event_dispatcher.go
+++ eventos/pkg/events/event_dispatcher.go
@@ -1,1 +1,34 @@
 package events
+
+import "errors"
+
+var ErrHandlerAlreadyRegistered = errors.New("Handler already registered")
+
+// EventDispatcher is responsible for managing event handlers and dispatching events to them.
+// It maintains a map where the keys are event names and the values are slices of EventHandlerInterface.
+// This allows multiple handlers to be registered for a single event.
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+// NewEventDispatcher creates a new EventDispatcher instance.
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{
+		handlers: make(map[string][]EventHandlerInterface),
+	}
+}
+
+// Register adds a handler for a given event.
+func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return ErrHandlerAlreadyRegistered
+			}
+		}
+	}
+
+	ed.handlers[eventName] = append(ed.handlers[eventName], handler)
+	return nil
+}
--- eventos/pkg/events/interfaces.go
+++ eventos/pkg/events/interfaces.go
@@ -1,1 +1,26 @@
 package events
+
+import "time"
+
+type EventInterface interface {
+	getName() string
+	getDataTime() time.Time
+	getPayload() interface{}
+}
+
+type EventHandlerInterface interface {
+	Handle(event EventInterface)
+}
+
+type EventDispatcherInterface interface {
+	// Register an event with a handler
+	Register(eventName string, handler EventHandlerInterface) error
+	// Dispatch an event
+	Dispatch(event EventInterface) error
+	// Remove an event handler
+	Remove(eventName string, handler EventHandlerInterface) error
+	// Check if an event has a handler
+	Has(eventName string, handler EventHandlerInterface) bool
+	// Get all handlers for an event
+	Claer() error
+}
